41d84354a6cdc53a622069e88d5317ea02a89c29
Cody D'Orazio <cxdorazio@gmail.com>
2019-05-04 20:28:10 -0400

    Initial commit

---
+++ .idea/sportList.js
@@ -0,0 +1,15 @@
+<!--Author: Cody D'Orazio
+    Assignment: Search filter project
+    Date last edited: 5/4/19
+    -->
+
+var sport = [
+    'Lacrosse',
+    'Soccer',
+    'Swimming',
+    'Polo',
+    'Baseball',
+    'Football',
+    ];
+
+ul = document.getElementById("sport-list")
